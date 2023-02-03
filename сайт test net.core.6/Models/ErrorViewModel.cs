19be4d2b28ef5193f0421ad92cc91bc88affed93
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2023-02-03 21:54:43 +0300

    test

---
+++ сайт test net.core.6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace сайт_test_net.core._6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
